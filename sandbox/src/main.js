6344a3fb9f9e69861d5a6ca0ba44f3427fca8a04
Sijibomi Olusunmbola <sijibomiolusunmbola@gmail.com>
2026-02-20 19:23:58 +0100

    add arcade physics to config

--- sandbox/src/main.js
+++ sandbox/src/main.js
@@ -6,6 +6,13 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -24,8 +31,18 @@ function preload() {
     this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
+
+let platforms;
+
 function create() {
-    this.add.image(400, 300, 'sky')
-    this.add.image(400, 300, 'star')
+    this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 function update() {}

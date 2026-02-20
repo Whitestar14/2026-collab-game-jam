b291de149a05337e30eaf353b0d0a7f75560956b
Sijibomi Olusunmbola <sijibomiolusunmbola@gmail.com>
2026-02-20 20:53:47 +0100

    sandbox: add collision, score tracking, reward system

--- sandbox/src/main.js
+++ sandbox/src/main.js
@@ -33,10 +33,26 @@ function preload() {
 }
 
 let platforms;
+/**
+ * @type {{ setBounce: (arg0: number) => void; setCollideWorldBounds: (arg0: boolean) => void; body: { setGravityY: (arg0: number) => void; touching: { down: any; }; }; setVelocityX: (arg0: number) => void; anims: { play: (arg0: string, arg1: boolean | undefined) => void; }; setVelocityY: (arg0: number) => void; }}
+ */
+let player;
+/**
+ * @type {{ left: { isDown: any; }; right: { isDown: any; }; up: { isDown: any; }; }}
+ */
+let cursors;
+let stars;
 
+let score = 0;
+let scoreText;
+
+/**
+ * @this {{ preload: () => void; create: () => void; update: () => void; }}
+ */
 function create() {
     this.add.image(400, 300, 'sky');
 
+    //create a static group holding all the static platform objects
     platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
@@ -44,5 +60,81 @@ function create() {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+
+    // create the sprite
+    player = this.physics.add.sprite(100, 450, 'dude');
+
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    // this sets how strong the player should experience gravity, the higher it is,
+    // the faster it falls in freefall
+    player.body.setGravityY(100);
+
+    // add collision with other game objects like the platform
+    this.physics.add.collider(player, platforms);
+
+    // add some animations the sprite can use
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'dude', frame: 4 }],
+        frameRate: 20
+    })
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    //instantiate keyboard manager with phaser
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars=this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    stars.children.iterate((/** @type {{ setBounceY: (arg0: number) => void; }} */ child) => {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8))
+    })
+
+    this.physics.add.collider(stars, platforms);
+
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: "#000000", })
 }
-function update() {}
+
+function update() {
+    // update cursors in the update clause
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn')
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-400);
+    }
+
+    this.physics.add.overlap(player, stars, collectStar, null, this)
+}
+
+function collectStar(player, star) {
+    star.disableBody(true, true);
+    score += 10;
+    scoreText.setText('score: ' + score)
+}
